ec6d0e59ea2dc9eb32c9b03f8a155cf9f669a8b6
Carlomos7 <cs525@njit.edu>
2025-02-20 17:40:50 -0500

    Feat: setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygrits",
+    version="0.1.0",
+    packages=find_packages(include=["app"]),
+    install_requires=[
+        "click",
+        "colorama",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pygrits=app.cli.commands:cli",  # Changed from pygrits.cli to app.cli
+        ],
+    },
+)
